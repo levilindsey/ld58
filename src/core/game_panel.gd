19c43b68f9f9db539b2058aab11df6dc1e0f2923
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-10-03 20:39:10 -0700

    Add initial player and level scenes

--- src/core/game_panel.gd
+++ src/core/game_panel.gd
@@ -4,3 +4,10 @@ extends Node2D
 
 func _ready() -> void:
     G.game_panel = self
+
+    get_viewport().size_changed.connect(_on_viewport_size_changed)
+    _on_viewport_size_changed()
+
+
+func _on_viewport_size_changed() -> void:
+    var size := self.get_viewport().get_visible_rect().size
---
+++ src/level_chunk.gd
@@ -0,0 +1,2 @@
+class_name LevelChunk
+extends Node2D
