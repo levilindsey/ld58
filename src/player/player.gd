2ffb42c74bb8333f4581a1ac7ae98862b4359253
Jake Shoudy <jshoudy@uw.edu>
2025-10-04 10:32:49 -0700

    Add abduction w/ beam
    Add some state management to keep track of pedestrians currently in the beam and the pedestrians that have successfully been collected. Adjust height as pedestrians are getting closer to abduction

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,12 +1,15 @@
 class_name Player
 extends CharacterBody2D
 
-const MAX_SPEED = 400
 const ACCELERATION = 1000
 const MAX_ROTATION = PI / 8
-const DAMPING_FACTOR = 0.95
+const DAMPING_FACTOR_HORIZONTAL = 0.95
+const DAMPING_FACTOR_VERTICAL = 0.8
+var max_speed = 400
 var gravity = 0
 var pedestrians_in_beam = {}
+var pedestrians_collected = {}
+var is_beaming = false
 
 func _ready() -> void:
     G.player = self
@@ -14,37 +17,54 @@ func _ready() -> void:
 func _physics_process(delta):
     var previous_pos = position
     handle_movement(delta)
+    handle_beam()
+    abduct_pedestrians(previous_pos)
     
+func handle_beam():
     var beam = get_node("TractorBeam")
     var beamCollisionArea = %TractorBeamCollisionPolygon
     if Input.is_action_pressed("ui_select"):
+        is_beaming = true
         beam.visible = true
         beamCollisionArea.set_deferred("disabled", false)
+        max_speed = 50
     if Input.is_action_just_released("ui_select"):
+        is_beaming = false
         beam.visible = false
         beamCollisionArea.set_deferred("disabled", true)
-        
+        max_speed = 400
+        for ped in pedestrians_in_beam:
+            ped.on_beam_end()
+        pedestrians_in_beam.clear()
+
+func abduct_pedestrians(previous_pos):
     var player_x_delta = position.x - previous_pos.x
-    for ped in pedestrians_in_beam:
+    var player_bottom_edge = %PlayerBodyCollisionShape.shape.get_height() / 2.0 + global_position.y
+    for ped in pedestrians_in_beam.duplicate_deep():
+        var ped_height_offset = ped.get_node("CollisionShape2D").shape.get_height() / 2
         ped.position.x = move_toward(ped.position.x, position.x, 0.1) + player_x_delta
-        ped.position.y = move_toward(ped.position.y, position.y, 0.5)
-        
+        ped.position.y = move_toward(ped.position.y, player_bottom_edge + ped_height_offset, 0.5)
+        if ped.position.y == player_bottom_edge + ped_height_offset:
+            # The pedestrian has been collected
+            pedestrians_collected[ped] = true
+            ped.visible = false
+            pedestrians_in_beam.erase(ped)
 
 func handle_movement(delta):
     if Input.is_action_pressed("ui_left"):
-        velocity.x = clamp(velocity.x + ACCELERATION * delta * -1, MAX_SPEED * -1, MAX_SPEED)
-        rotation = clamp(velocity.x / MAX_SPEED * MAX_ROTATION, MAX_ROTATION * -1, MAX_SPEED)
+        velocity.x = clamp(velocity.x + ACCELERATION * delta * -1, max_speed * -1, max_speed)
+        rotation = clamp(velocity.x / max_speed * MAX_ROTATION, MAX_ROTATION * -1, max_speed)
     if Input.is_action_pressed("ui_right"):
-        velocity.x = clamp(velocity.x + ACCELERATION * delta, MAX_SPEED * -1, MAX_SPEED)
-        rotation = clamp(velocity.x / MAX_SPEED * MAX_ROTATION, MAX_ROTATION * -1, MAX_ROTATION)
-    if Input.is_action_pressed("ui_up"):
-        velocity.y = clamp(velocity.y + ACCELERATION * delta * -1, MAX_SPEED * -1, MAX_SPEED)
-    if Input.is_action_pressed("ui_down"):
-        velocity.y = clamp(velocity.y + ACCELERATION * delta, MAX_SPEED * -1, MAX_SPEED)
+        velocity.x = clamp(velocity.x + ACCELERATION * delta, max_speed * -1, max_speed)
+        rotation = clamp(velocity.x / max_speed * MAX_ROTATION, MAX_ROTATION * -1, MAX_ROTATION)
+    if Input.is_action_pressed("ui_up") and not is_beaming:
+        velocity.y = clamp(velocity.y + ACCELERATION * delta * -1, max_speed * -1, max_speed)
+    if Input.is_action_pressed("ui_down") and not is_beaming:
+        velocity.y = clamp(velocity.y + ACCELERATION * delta, max_speed * -1, max_speed)
 
-    if not is_movement_action_pressed():
-        velocity.x = velocity.x * DAMPING_FACTOR
-        velocity.y = velocity.y * DAMPING_FACTOR
+    if not is_movement_action_pressed() or is_beaming:
+        velocity.x = velocity.x * DAMPING_FACTOR_HORIZONTAL
+        velocity.y = velocity.y * DAMPING_FACTOR_VERTICAL
         # TODO: revisit and add wobbling when decelerating?
         rotation = 0
 
