76e0848b0e21b0e93ad42c09fb97b4d4d92deefe
Jake Shoudy <jshoudy@uw.edu>
2025-10-04 00:25:25 -0700

    Add acceleration on player movement`
    Add better movement with acceleration/deceleration, including some rotation.

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,9 +1,38 @@
 class_name Player
 extends CharacterBody2D
 
-var speed = 400
+var max_speed = 400
+var acceleration = 1000
+var max_rotation = PI / 8
+var damping_factor = 0.95
+var gravity = 0
 
 func _physics_process(delta):
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_dir * speed
-	var collision = move_and_collide(velocity * delta)
+    if Input.is_action_pressed("ui_left"):
+        velocity.x = clamp(velocity.x + acceleration * delta * -1, max_speed * -1, max_speed)
+        rotation = clamp(velocity.x / max_speed * max_rotation, max_rotation * -1, max_rotation)
+    if Input.is_action_pressed("ui_right"):
+        velocity.x = clamp(velocity.x + acceleration * delta, max_speed * -1, max_speed)
+        rotation = clamp(velocity.x / max_speed * max_rotation, max_rotation * -1, max_rotation)
+    if Input.is_action_pressed("ui_up"):
+        velocity.y = clamp(velocity.y + acceleration * delta * -1, max_speed * -1, max_speed)
+    if Input.is_action_pressed("ui_down"):
+        velocity.y = clamp(velocity.y + acceleration * delta, max_speed * -1, max_speed)
+
+    if not is_movement_action_pressed():
+        velocity.x = velocity.x * damping_factor
+        velocity.y = velocity.y * damping_factor
+        # TODO: revisit and add wobbling when decelerating?
+        rotation = 0
+        
+    if abs(velocity.x) < 1:
+        velocity.x = 0
+    
+    if abs(velocity.y) < 1:
+        velocity.y = 0
+        
+    velocity.y += gravity * delta
+    var collision = move_and_collide(velocity * delta)
+
+func is_movement_action_pressed():
+    return Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down")
