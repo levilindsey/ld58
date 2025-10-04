9dbb4a00eafee71bf5cdf4243c0b333328fd1504
Jake Shoudy <jshoudy@uw.edu>
2025-10-03 21:40:52 -0700

    Add simple player movement
    1) Add simple left, right, down, up movement to player
    2) Update terrain to have physics

--- src/player.gd
+++ src/player.gd
@@ -1,2 +1,9 @@
 class_name Player
-extends Node2D
+extends CharacterBody2D
+
+var speed = 400	
+
+func _physics_process(delta):
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
+	var collision = move_and_collide(velocity * delta)
