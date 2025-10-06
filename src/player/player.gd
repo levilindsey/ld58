7ef774ff7af82546a423bd8ac4aa2f486b2aabdf
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2025-10-06 11:18:46 -0700

    Fix bad reference

--- src/player/player.gd
+++ src/player/player.gd
@@ -26,7 +26,7 @@ func _ready() -> void:
 
 
 func reset() -> void:
-    for child in pedestrians_in_beam.get_children():
+    for child in %EnemiesInBeam.get_children():
         child.queue_free()
     pedestrians_in_beam.clear()
     is_beaming = false
